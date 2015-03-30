0fd486802d0fd09af46603797959fda2859054f4
Nop Jiarathanakul <nop.jiarathanakul@autodesk.com>
2015-03-30 11:38:22 -0700

    allow loading by url param and fixed doc tree loading

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,27 @@
 (function (document) {
   "use strict";
 
+  var getParameterByName = function(name) {
+      name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+      var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+          results = regex.exec(location.search);
+      return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  };
+
   var app = document.querySelector("#app");
   app.addEventListener("template-bound", function() {
     console.log("Polymer Template Ready");
   });
-  // app.svfURL = "data/engineraw/0.svf";
-  app.docURL = "data/Displayline/output/bubble.json";
-  // app.docURL = "data/gears/output/bubble.json";
+
+  app.svfURL = getParameterByName("svf");
+  app.docURL = getParameterByName("doc");
+
+  if (!app.svfURL && !app.docURL) {
+    app.svfURL = "data/engineraw/0.svf";
+    // app.docURL = "data/Displayline/output/bubble.json";
+    // app.docURL = "data/gears/output/bubble.json";
+  }
+
   window.NOP_APP = app;
 
 // wrap document so it plays nice with other libraries
